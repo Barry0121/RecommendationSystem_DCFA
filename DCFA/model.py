9f7df1bb845076d88c17d7eb1f6a93e9bb0107ef
Barry Xue <66921377+Barry0121@users.noreply.github.com>
2022-03-10 22:46:07 -0800

    First Commit: Remove data and setting files

---
+++ DCFA/model.py
@@ -0,0 +1,6 @@
+from Library import readdata
+from Library import readdata_time
+from Library import evaluation_F1
+from Library import evaluation_NDCG
+from Library import save_result
+from Library import read_feature
